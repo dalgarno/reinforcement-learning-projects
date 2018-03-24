a004e4f3c3e2b7c4b8130ee08c1563cf62516220
dalgarno <alasdair.dalgarno@gmail.com>
2018-03-24 14:25:44 +0000

    Delete old files

--- OpenAI_Gym/Classic_Control/agent.py
+++
@@ -1,8 +0,0 @@
-class Agent(object):
-    def __init__(self, env, alpha=0.01, eps=0.2, gamma=0.98):
-        super().__init__()
-        self.env = env
-        self.eps = eps
-        self.gamma = gamma
-        self.alpha = alpha
-        self.num_episodes = None
